7f5ad00afc0ddb0b8d1f33353eabdfde720ff5db
乾坤 <468834419@qq.com>
2022-09-06 16:33:35 +0800

    修改测试

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ import {
 import { config } from './config/index.js'
 import { toLowerLine, getColor } from './src/utils/index.js'
 
+console.log(213)
 const getAggregatedData = async () => {
 
   const weekList = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
